173afcf32bd4fb2a05ae26ff0cdb2ee9cb5cb66f
jagrati gupta <123jagratigupta@gmail.com>
2024-11-02 20:01:47 +0530

    JG: Updates - Fetch & display the question - Handle next - Handle re-start game - Added skeleton

--- app/game/page.js
+++ app/game/page.js
@@ -1,12 +1,54 @@
 "use client";
-
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { QuizCard } from "../components/Game/QuizCard";
 import { TimerComponent } from "../components/Game/TimerComponent";
 import { ScoreBlock } from "../components/Game/ScoreBlock";
+import axios from "axios";
+import { Skeleton } from "../components/Skeleton";
 
 export default function Game() {
-  const [isQuizCompleted, setIsQuizCompleted] = useState(true);
+  const [questions, setQuestions] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [isQuizCompleted, setIsQuizCompleted] = useState(false);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+
+  // Function fetch all the quiz questions
+  const handleGetAllQuestions = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.get('/api/quiz');
+      if (response.status === 200) {
+        const data = await response.data;
+        setQuestions(data);
+      }
+      else {
+        console.error('Unexpected response status:', response.status);
+      }
+    } catch (error) {
+      console.error('Internal server error', error.response?.statusText || error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  // Function show next question
+  const handleNext = () => {
+    if (currentQuestionIndex < questions.length - 1) {
+      setCurrentQuestionIndex((currentQuestionIndex) => currentQuestionIndex + 1);
+    } else {
+      setIsQuizCompleted(true);
+    }
+  }
+
+  // Function restart the game
+  const handleRestart = () => {
+    setCurrentQuestionIndex(0);
+    setIsQuizCompleted(false);
+  }
+
+  useEffect(() => {
+    handleGetAllQuestions();
+  }, []);
 
   return (
     <section className="upraised_game_container flex flex-col bg-white">
@@ -15,22 +57,17 @@ export default function Game() {
 
       {/* quiz questions container */}
       <div className="bg-white rounded-tl-[60px] rounded-tr-[60px] flex-1 pt-10 px-3">
-
-        {
-          isQuizCompleted ?
-            <ScoreBlock correctCount={2} inCorrectCount={7} />
-            :
-            <>
+        {isQuizCompleted
+          ? <ScoreBlock correctCount={2} inCorrectCount={7} />
+          :
+          loading
+            ? <Skeleton />
+            : <>
               {/* timer*/}
-              <TimerComponent currentCount={1} totalCount={5} />
-
+              <TimerComponent currentCount={currentQuestionIndex + 1} totalCount={questions?.length} />
               {/* question with answer */}
               <div className="px-2">
-                <QuizCard quizData={{
-                  question: "What are your favorite fruits?",
-                  options: ["Apple", "Banana", "Cherry", "Date"],
-                  type: "single"
-                }} />
+                <QuizCard quizData={questions[currentQuestionIndex]} />
               </div>
             </>
         }
@@ -39,8 +76,8 @@ export default function Game() {
       {/* next button */}
       <div className="w-full bg-white p-4">
         {isQuizCompleted
-          ? <button className="upraised_button relative text-center">Start Again</button>
-          : <button className="upraised_button relative text-center">
+          ? <button className="upraised_button relative text-center" onClick={handleRestart}>Start Again</button>
+          : <button className="upraised_button relative text-center" onClick={handleNext}>
             Next<span className="absolute right-10">&#8594;</span>
           </button>
         }
