192f11de518cb1fe0fcc1fc5e765668c12362f1e
jagrati gupta <123jagratigupta@gmail.com>
2024-11-02 20:00:33 +0530

    JG: Created quiz dummy data

---
+++ app/data/quiz.js
@@ -0,0 +1,82 @@
+const questions = [
+  {
+    question: "What does HTML stand for?",
+    options: [
+      "Hyper Text Markup Language",
+      "Hyperlinks and Text Markup Language",
+      "High Text Markup Language",
+      "Hyper Text Multi Language"
+    ],
+    type: "single",
+    correct_answer: "Hyper Text Markup Language"
+  },
+  {
+    question: "Which of the following is not a JavaScript data type?",
+    options: [
+      "String",
+      "Number",
+      "Boolean",
+      "Character"
+    ],
+    type: "single",
+    correct_answer: "Character"
+  },
+  {
+    question: "Which CSS property is used to change the text color?",
+    options: [
+      "color",
+      "font-color",
+      "text-color",
+      "bgcolor"
+    ],
+    type: "single",
+    correct_answer: "color"
+  },
+  {
+    question: "Which HTML element is used to define an internal style sheet?",
+    options: [
+      "<style>",
+      "<css>",
+      "<script>",
+      "<styleSheet>"
+    ],
+    type: "single",
+    correct_answer: "<style>"
+  },
+  {
+    question: "What is the correct syntax for adding an image in HTML?",
+    options: [
+      "<img src='image.jpg'>",
+      "<image src='image.jpg'>",
+      "<img href='image.jpg'>",
+      "<img src='image.jpg'/>"
+    ],
+    type: "single",
+    correct_answer: "<img src='image.jpg'>",
+    image: "https://example.com/image_syntax_example.png"
+  },
+  {
+    question: "Which of the following React JS frameworks is used for building user interfaces?",
+    options: [
+      "Redux",
+      "Remix",
+      "Next JS",
+      "Laravel"
+    ],
+    type: "multiple",
+    correct_answer: ["Remix", "Next JS"]
+  },
+  {
+    question: "Which of the following are front-end technologies?",
+    options: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Python"
+    ],
+    type: "multiple",
+    correct_answer: ["HTML", "CSS", "JavaScript"]
+  }
+];
+
+export default questions;
