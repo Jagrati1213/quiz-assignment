32e10a2889d2a19917488f3bdebc06b9c7a18a4f
jagrati gupta <123jagratigupta@gmail.com>
2024-11-02 15:25:27 +0530

    JG: Added custom color name

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,17 @@ module.exports = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        upraisedBlack: "#222222",
+        upraisedOrange: "#FF3B3C",
+        upraisedGreen: "#44B77B",
+        upraisedGray: "#F3F4FA",
+        upraisedDarkRed: "#FF3B3F",
+        upraisedLavender: "#AF9CF3",
+      },
+      backgroundImage: {
+        upraisedLinear: "linear-gradient(180deg, rgba(175, 156, 243, 0) 7.92%, #AF9CF3 86.48%)",
+        upraisedLinearOrange: "linear-gradient(180deg, rgba(175, 156, 243, 0) 7.92%, #FF3B3C 86.48%)",
+        upraisedGameImage: "url('/backImage.svg')"
       },
     },
   },
