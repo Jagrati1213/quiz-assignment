bd1ef9f6a560685cfc2bcef777c6e5fe72508350
jagrati gupta <123jagratigupta@gmail.com>
2024-11-03 13:38:18 +0530

    JG: Added timer animation

--- app/components/Game/TimerComponent/index.jsx
+++ app/components/Game/TimerComponent/index.jsx
@@ -1,20 +1,32 @@
-import React from "react";
+import React, { useMemo } from "react";
 
-export function TimerComponent({ currentCount, totalCount }) {
+export function TimerComponent({ remainingTime, currentCount, totalCount }) {
+  const timerColor = remainingTime > 5 ? "#44B77B" : "#FF0000";
+
+  // Calculate the timer
+  const gaugeValue = useMemo(() => {
+    return (15 - remainingTime) * (360 / 15);
+  }, [remainingTime]);
+
   return (
     <div className="flex flex-col items-center justify-center">
-      <div className="relative -top-[80px] w-28 h-28 flex items-center justify-center bg-white rounded-full">
-        <div
-          className="absolute w-[85%] h-[85%] rounded-full border-8 border-upraisedGreen"
-          style={{
-            clipPath: "circle(50% at 50% 50%)",
-            animation: "countdown 5s linear infinite",
-          }}
-        ></div>
-        <h2 className="text-4xl font-bold italic">
-          {currentCount}
-          <span className="text-gray-400 text-xl">/{totalCount}</span>
-        </h2>
+      <div
+        className="w-32 h-32 rounded-full flex justify-center items-center relative -top-[80px] transition-all"
+        style={{
+          backgroundImage: `conic-gradient(
+          #D3D3D3 0deg,
+          #D3D3D3 ${gaugeValue}deg,
+          ${timerColor} ${gaugeValue}deg,
+          ${timerColor} 360deg
+        )`,
+        }}
+      >
+        <div className="bg-white w-28 h-28 rounded-full flex justify-center items-center">
+          <h2 className="text-4xl font-bold italic">
+            {currentCount}
+            <span className="text-gray-400 text-xl">/{totalCount}</span>
+          </h2>
+        </div>
       </div>
     </div>
   );
