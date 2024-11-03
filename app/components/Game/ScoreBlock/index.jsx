c4fd64b7c2f3e5f97bf4c294b0eab8ed51dbffd9
jagrati gupta <123jagratigupta@gmail.com>
2024-11-03 11:20:45 +0530

    JG: Set the score percentage dynamic

--- app/components/Game/ScoreBlock/index.jsx
+++ app/components/Game/ScoreBlock/index.jsx
@@ -1,13 +1,23 @@
-import React from "react";
+"use client";
+
+import React, { useMemo } from "react";
 import { Gauge } from "./Gauge";
 
 export function ScoreBlock({ correctCount, inCorrectCount }) {
+  // Calculate the percentage of scoring
+  const correctCountPercentage = useMemo(() => {
+    const totalQuestions = correctCount + inCorrectCount;
+    const percentage =
+      totalQuestions > 0 ? (correctCount / totalQuestions) * 100 : 0;
+    return Math.round(percentage);
+  }, [correctCount, inCorrectCount]);
+
   return (
     <div className="font-sans flex flex-col justify-between gap-6">
       <h3 className="text-center text-2xl font-bold">Your result</h3>
 
       <div className="flex justify-center">
-        <Gauge percentage={70} />
+        <Gauge percentage={correctCountPercentage} />
       </div>
 
       <div className="flex flex-col gap-4 w-full">
